77d17d703e7454f4149c137a729a21942a867330
young <young.wang@antcosa.com>
2018-08-22 17:39:06 +0800

    fix 无法捕获在权限验证阶段的panic

--- lib.go
+++ lib.go
@@ -2,7 +2,7 @@
 Beego Test Suite
 
 a code coverage friendly http test framework
- */
+*/
 package beetest
 
 import (
@@ -12,31 +12,32 @@ import (
 	"strconv"
 	"strings"
 
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"net/http/httptest"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
-	"net/http/httptest"
-	"encoding/json"
-	"bytes"
-	"encoding/xml"
 )
 
 type Tester struct {
-	req *http.Request
-	ctx *context.Context
+	req     *http.Request
+	ctx     *context.Context
 	control beego.ControllerInterface
-	resp *httptest.ResponseRecorder
-	params map[string]string
+	resp    *httptest.ResponseRecorder
+	params  map[string]string
 }
 
 func init() {
 	beego.BConfig.CopyRequestBody = true
 }
 
-func NewTester() *Tester{
+func NewTester() *Tester {
 	return new(Tester)
 }
 
-func (t *Tester) Reset() *Tester{
+func (t *Tester) Reset() *Tester {
 	t.req = nil
 	t.ctx = nil
 	t.resp = nil
@@ -45,77 +46,79 @@ func (t *Tester) Reset() *Tester{
 func (t *Tester) Params(p map[string]string) *Tester {
 	t.params = p
 	return t
-	}
+}
 func (t *Tester) Controller(ctrl beego.ControllerInterface) *Tester {
 	t.control = ctrl
 	return t
 }
 
 func (t *Tester) Get(uri string) *Tester {
-	t.request("GET",uri,nil,"application/json")
+	t.request("GET", uri, nil, "application/json")
 	return t
 }
 
 func (t *Tester) Delete(uri string) *Tester {
-	t.request("DELETE",uri,nil,"application/json")
+	t.request("DELETE", uri, nil, "application/json")
 	return t
 }
 
-func (t *Tester) PutJSON(uri string,body ...interface{}) *Tester {
+func (t *Tester) PutJSON(uri string, body ...interface{}) *Tester {
 	var b *bytes.Buffer = nil
 	if len(body) == 1 {
 		bo, _ := json.Marshal(body[0])
 		b = bytes.NewBuffer(bo)
 	}
-	t.request("PUT",uri,b,"application/json")
+	t.request("PUT", uri, b, "application/json")
 	return t
 }
 
-
-func (t *Tester) PutXML(uri string,body ...interface{}) *Tester {
+func (t *Tester) PutXML(uri string, body ...interface{}) *Tester {
 	var b *bytes.Buffer = nil
 	if len(body) == 1 {
 		bo, _ := xml.Marshal(body[0])
 		b = bytes.NewBuffer(bo)
 	}
-	t.request("PUT",uri,b,"application/xml")
+	t.request("PUT", uri, b, "application/xml")
 	return t
 }
 
-func (t *Tester) PostJSON(uri string,body ...interface{}) *Tester {
+func (t *Tester) PostJSON(uri string, body ...interface{}) *Tester {
 	var b *bytes.Buffer = nil
 	if len(body) == 1 {
 		bo, _ := json.Marshal(body[0])
 		b = bytes.NewBuffer(bo)
 	}
-	return t.request("POST",uri,b,"application/json")
+	return t.request("POST", uri, b, "application/json")
 }
 
-func (t *Tester) PostXML(uri string,body ...interface{}) *Tester {
+func (t *Tester) PostXML(uri string, body ...interface{}) *Tester {
 	var b *bytes.Buffer = nil
 	if len(body) == 1 {
 		bo, _ := xml.Marshal(body[0])
 		b = bytes.NewBuffer(bo)
 	}
-	return t.request("POST",uri,b,"application/xml")
+	return t.request("POST", uri, b, "application/xml")
 }
 
 func (t *Tester) Request(r *http.Request) *Tester {
-	t.req  = r
+	t.req = r
 	return t
 }
 
-func (t *Tester) request(method string,path string,reader io.Reader,contentType string) *Tester{
-	if t.req == nil{
-		t.req ,_ = http.NewRequest(method,path,reader)
+func (t *Tester) request(method string, path string, reader io.Reader, contentType string) *Tester {
+	if t.req == nil {
+		t.req, _ = http.NewRequest(method, path, reader)
 	}
-	t.req.Header.Set("Content-Type",contentType)
+	t.req.Header.Set("Content-Type", contentType)
 	return t
 }
 
-func (t *Tester) Run(h func()) *Tester{
-	recover := 	httptest.NewRecorder()
-	t.initContext(t.req,recover)
+func (t *Tester) Run(h func()) *Tester {
+	recover := httptest.NewRecorder()
+	if t.initContext(t.req, recover) {
+		t.resp = recover
+		return t
+	}
 	t.run(h)
 	t.resp = recover
 	t.control.Finish()
@@ -149,14 +152,20 @@ func (t *Tester) Receive(data interface{}) error {
 	return nil
 }
 
-func (t *Tester) initContext(r *http.Request,rw http.ResponseWriter) {
+func (t *Tester) initContext(r *http.Request, rw http.ResponseWriter) (stopRun bool) {
+	defer func() {
+		if err := recover(); err != nil {
+			if err != beego.ErrAbort {
+				panic(err)
+			} else {
+				stopRun = true
+			}
+		}
+	}()
 	ctx := context.NewContext()
 	ctx.Request = r
-	ctx.Reset(rw,r)
+	ctx.Reset(rw, r)
 
-	if beego.BConfig.RecoverFunc != nil {
-		defer beego.BConfig.RecoverFunc(ctx)
-	}
 	var urlPath = r.URL.Path
 	if !beego.BConfig.RouterCaseSensitive {
 		urlPath = strings.ToLower(urlPath)
@@ -167,7 +176,6 @@ func (t *Tester) initContext(r *http.Request,rw http.ResponseWriter) {
 		http.Error(rw, "Method Not Allowed", 405)
 		return
 	}
-
 
 	if r.Method != http.MethodGet && r.Method != http.MethodHead {
 		if beego.BConfig.CopyRequestBody && !ctx.Input.IsUpload() {
@@ -181,16 +189,12 @@ func (t *Tester) initContext(r *http.Request,rw http.ResponseWriter) {
 			ctx.Input.SetParam(strconv.Itoa(k), v)
 		}
 	}
-	for k,v := range t.params {
-		ctx.Input.SetParam(k,v)
+	for k, v := range t.params {
+		ctx.Input.SetParam(k, v)
 	}
-		//Invoke the request handler
-		//call the controller init function
-		t.control.Init(ctx, "", "", t.control)
-		t.control.Prepare()
-
+	//Invoke the request handler
+	//call the controller init function
+	t.control.Init(ctx, "", "", t.control)
+	t.control.Prepare()
+	return false
 }
-
-
-
-
